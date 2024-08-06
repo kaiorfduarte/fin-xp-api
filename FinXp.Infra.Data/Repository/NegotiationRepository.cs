d92e65085adb3a1816deacddb81c71cf3adbce6a
Kaio Duarte <kn.schiffer@gmail.com>
2024-08-06 02:03:14 -0300

    add transaction + k6 update test

--- FinXp.Infra.Data/Repository/NegotiationRepository.cs
+++ FinXp.Infra.Data/Repository/NegotiationRepository.cs
@@ -11,18 +11,28 @@ public class NegotiationRepository(ISqlConnection _sqlConnection) : INegotiation
     public async Task<bool> SaveBuyNegotiationDataAsync(NegotiationProduct negotiationProduction)
     {
         await using NpgsqlConnection connection = _sqlConnection.CreateConnection();
+        connection.Open();
+        await using var tran = connection.BeginTransaction();
 
-        return await connection.ExecuteScalarAsync<bool>(
+        var result = await connection.ExecuteScalarAsync<bool>(
             "SELECT SaveBuyNegotiation(@ClientId, @ProductId, @Quantity);",
             negotiationProduction);
+
+        tran.Commit();
+        return result;
     }
 
     public async Task<bool> SaveSellNegotiationDataAsync(NegotiationProduct negotiationProduction)
     {
         await using NpgsqlConnection connection = _sqlConnection.CreateConnection();
+        connection.Open();
+        await using var tran = connection.BeginTransaction();
 
-        return await connection.ExecuteScalarAsync<bool>(
+        var result = await connection.ExecuteScalarAsync<bool>(
             "SELECT SaveSellNegotiation(@ClientId, @ProductId, @Quantity);",
             negotiationProduction);
+
+        tran.Commit();
+        return result;
     }
 }
--- loadtest.js
+++ loadtest.js
@@ -1,23 +1,44 @@
+import { check } from 'k6';
 import http from 'k6/http';
 
+const only422Callback = http.expectedStatuses(422);
+
 export const options = {
-    vus: 10,
-    duration: '30s',
+    vus: 1,
+    duration: '10s',
     thresholds: {
-        http_req_failed: ['rate<0.01'], // http errors should be less than 1%
-        http_req_duration: ['p(95)<200'], // 95% of requests should be below 200ms
+        'http_req_duration{type:NegotiationSave}': ['p(99)<100'], 
+        'http_req_duration{type:GetClientProductList}': ['p(99)<100'],
     },
 };
 
 export default function () {
     let data = {
-        ProductId: 3,
-        ClientId: 1,
-        Quantity: 1,
-        OperationTypeId: 1
+        ProductId: Math.floor(Math.random() * 5) + 1, //gera de 1 a 5 os produtos
+        ClientId: Math.floor(Math.random() * 5) + 1, //gera de 1 a 5 os clientes
+        Quantity: Math.floor(Math.random() * 5) + 1, //gera de 1 a 5 as quantidades
+        OperationTypeId: Math.floor(Math.random() * 2) + 1 //1 para compra e 2 para venda
     };
 
-    http.post('http://localhost:8080/Negotiation/Save', JSON.stringify(data), {
-        headers: { 'Content-Type': 'application/json' }
+    const response = http.post('http://localhost:8080/Negotiation/Save', JSON.stringify(data), {
+        headers: { 'Content-Type': 'application/json' }, tags: { type: 'NegotiationSave' }, responseCallback: only422Callback,
+    });
+
+    check(response, {
+        'save is status 204': (r) => r.status === 204,
+        'save is status 201': (r) => r.status === 201,
+        'save is status 422': (r) => r.status === 422,
+        'save is status 500': (r) => r.status === 500,
+    });
+
+    const clientId = Math.floor(Math.random() * 5) + 1;
+    const responseGet = http.get(`http://localhost:8080/Product/GetClientProductList?clientId=${clientId}` , {
+        tags: { type: 'GetClientProductList' },
+    });
+
+    check(responseGet, {
+        'list is status 200': (r) => r.status === 200,
+        'list is status 204': (r) => r.status === 204,
+        'list is status 500': (r) => r.status === 500,
     });
 }
